aaaf3d1a7828d08ced1ae77edbdc51cd2215d72b
chmu7568 <christian.mueller@esolutions.de>
2025-12-19 14:50:24 +0100

    make ai cars drive on straigt lanes

--- game/entities/ai/ai_car_controller.gd
+++ game/entities/ai/ai_car_controller.gd
@@ -2,15 +2,17 @@ class_name AiCarController
 extends Node3D
 
 @export var distance_to_goal_tolerance: float = 1.0
+@export var distance_look_ahead: float = 5.0
 @export_range(0.0, 1.0) var speed_min: float
 @export_range(0.0, 1.0) var speed_max: float
 
 @onready var car: RayCastCar = get_parent()
 
-var target_checkpoint: Checkpoint
+var start_position := Vector3.ZERO
+var target_position := Vector3.FORWARD * 100.0
 var speed: float
 
-var _intrests: Array[RayCast3D]
+var _interests: Array[RayCast3D]
 var _distance_checker: RayCast3D
 
 
@@ -19,7 +21,7 @@ func _ready():
 
 	speed = randf_range(speed_min, speed_max)
 
-	_intrests.assign(find_children("IntrestVector*", "RayCast3D"))
+	_interests.assign(find_children("IntrestVector*", "RayCast3D"))
 	_distance_checker = get_node("DistanceChecker")
 
 
@@ -35,49 +37,34 @@ func _process(_delta):
 
 
 func is_on_goal_position() -> bool:
-	return (
-		global_position.distance_to(target_checkpoint.global_position) < distance_to_goal_tolerance
-	)
+	return global_position.distance_to(target_position) < distance_to_goal_tolerance
 
 
 # return car input, x = motor, y = steering
 func _get_input() -> Vector2:
 	var input := Vector2.ZERO
 
-	var to_target: Vector3 = (
-		(target_checkpoint.global_position - car.global_transform.origin).normalized()
-	)
+	var drive_to: Vector3 = _get_closest_point_on_lane()
+	var to_target: Vector3 = car.global_position.direction_to(drive_to)
+
+	DebugDraw3D.draw_arrow_ray(drive_to + Vector3.UP, Vector3.DOWN, 1.0, Color.RED)
 
 	# calculate where to drive
-	var intrest_vector := Vector3.ZERO
-	for intrest in _intrests:
-		var intrest_forward = -intrest.global_basis.z
-		var partially_intrest = intrest_forward.dot(to_target) * intrest_forward
+	var interest_vector := Vector3.ZERO
+	for interest in _interests:
+		var interest_forward = -interest.global_basis.z
+		var partially_interest = to_target.dot(interest_forward) * interest_forward
 
-		if intrest.is_colliding():
+		if interest.is_colliding():
 			continue
 
-		intrest_vector += partially_intrest
+		interest_vector += partially_interest
 
-	input.y = _get_steering(intrest_vector.normalized())
-
-	# TODO: refactor
-	if _distance_checker.is_colliding():
-		var collider_velocity := Vector3.ZERO
-		var collider := _distance_checker.get_collider() as RigidBody3D
-		if collider:
-			collider_velocity = collider.linear_velocity
-
-		var car_forwad = -car.global_basis.z
-		var car_forward_speed = car_forwad.dot(car.linear_velocity)
-		var target_speed = car_forwad.dot(collider_velocity) - car_forward_speed
-
-		input.x = speed * (clampf(target_speed, -1.0, 1.0))
-	else:
-		input.x = speed
+	input.y = _get_steering(interest_vector.normalized())
+	input.x = _get_speed()
 
 	DebugDraw3D.draw_arrow_ray(
-		car.global_position, -car.global_basis.z, input.x * 10.0, Color.RED, 0.1, true
+		car.global_position, interest_vector.normalized(), input.x * 10.0, Color.RED, 0.1, true
 	)
 
 	return input
@@ -96,3 +83,28 @@ func _get_steering(to_target: Vector3):
 	var angle: float = -atan2(forward.cross(to_target).y, forward.dot(to_target))  # radians, left -, right +
 	var steering: float = clamp(rad_to_deg(angle) / car.turn_degrees, -1.0, 1.0)  # normaliz and clamp angle
 	return steering
+
+
+func _get_speed() -> float:
+	if not _distance_checker.is_colliding():
+		return speed
+
+	var collider_velocity := Vector3.ZERO
+	var collider := _distance_checker.get_collider() as RigidBody3D
+	if collider:
+		collider_velocity = collider.linear_velocity
+
+	var car_forward = -car.global_basis.z
+	var car_forward_speed = car_forward.dot(car.linear_velocity)
+	var target_speed = car_forward.dot(collider_velocity) - car_forward_speed
+
+	return speed * (clampf(target_speed, -1.0, 1.0))
+
+
+func _get_closest_point_on_lane() -> Vector3:
+	var closest_point := Geometry3D.get_closest_point_to_segment(
+		car.global_position, start_position, target_position
+	)
+	var lane_direction := start_position.direction_to(target_position)
+	var look_ahead_point := closest_point + distance_look_ahead * lane_direction
+	return look_ahead_point
--- game/entities/ai/ai_car_manager.gd
+++ game/entities/ai/ai_car_manager.gd
@@ -6,33 +6,32 @@ extends Node3D
 @onready var ai_car_scene = preload("res://game/entities/ai/ai_car.tscn")
 
 var _controllers: Array[AiCarController]
-var _checkpoints: Array[Checkpoint]
+var _spawn_points: Array[Node3D]
+var _spawn_timer: float = 0.0
 
 
 func _ready():
 	assert(world)
 
-	for i in range(car_amount):
-		var car := ai_car_scene.instantiate() as RayCastCar
-		world.add_child(car)
-		car.global_position = Vector3(-200, 10, -200)
-		car.global_position -= (Vector3(floori(i / 20.0), 0, i % 20) * 3)
-
-	for controller in get_tree().get_nodes_in_group("ai_car_controller"):
-		if controller is AiCarController:
-			_controllers.append(controller)
-
-	for checkpoint in get_tree().get_nodes_in_group("checkpoint"):
-		if checkpoint is Checkpoint:
-			_checkpoints.append(checkpoint)
-
-	if not _checkpoints.is_empty():
-		for controller in _controllers:
-			controller.target_checkpoint = _checkpoints.front()
-
 
 func _process(_delta):
-	for controller in _controllers:
-		if not controller.is_on_goal_position():
-			continue
-		controller.target_checkpoint = controller.target_checkpoint.linked_points.pick_random()
+	if _controllers.size() >= car_amount:
+		return
+	if _spawn_timer < 3.0:
+		_spawn_timer += _delta
+		return
+	_spawn_timer = 0.0
+
+	for point in get_tree().get_nodes_in_group("car_spawn"):
+		_spwan_car(point.global_transform)
+
+
+func _spwan_car(car_transform: Transform3D):
+	var car := ai_car_scene.instantiate() as RayCastCar
+	world.add_child(car)
+	car.global_transform = car_transform
+
+	var controller: AiCarController = car.get_node("AiCarController")
+	controller.start_position = car.global_position
+	controller.target_position = car.global_position + 1000.0 * (-car.global_basis.z)
+	_controllers.append(controller)
