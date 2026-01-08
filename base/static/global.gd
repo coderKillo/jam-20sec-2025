202c7d05520ff4c3ce429a885868ba59d9efd541
chmu7568 <christian.mueller@esolutions.de>
2026-01-08 15:53:08 +0100

    make car spawn and despawn

--- base/static/global.gd
+++ base/static/global.gd
@@ -4,3 +4,5 @@ extends Node
 const RED_TRANSITION_TIME = 2.0
 const GREEN_TRANSITION_TIME = 2.0
 const GREEN_INTERVAL = 5.0
+
+const PLAYER_VIEW_DISTANCE = 60.0
--- game/entities/ai/ai_car_manager.gd
+++ game/entities/ai/ai_car_manager.gd
@@ -4,10 +4,14 @@ extends Node3D
 @export var world: Node3D
 @export var car_spawn: Node3D
 @export var spawn_time: float = 1.0
+@export var spawn_distance_in_view: float = 100.0
+@export var spawn_distance_out_of_view: float = 10.0
+@export var spawn_angle_to_player: float = 80.0
 
 @onready var ai_car_scene := preload("res://game/entities/ai/ai_car.tscn")
 
 var _controllers: Array[AiCarController]
+var _player_car: RayCastCar
 
 
 func _ready():
@@ -16,15 +20,56 @@ func _ready():
 	$Timer.timeout.connect(_on_spawn_timer_timeout)
 	$Timer.start(spawn_time)
 
+	_player_car = world.get_node("PlayerCar") as RayCastCar
+	assert(_player_car)
+
 
 func _on_spawn_timer_timeout():
+	_despawn_cars()
+
 	if _controllers.size() >= car_amount:
 		return
 
-	# for lane_spawn in get_tree().get_nodes_in_group("car_spawn"):
-	# 	_spwan_car(lane_spawn)
-	for spawn in car_spawn.find_children("CarSpawner*", "CarSpawnLane"):
-		_spwan_car(spawn)
+	for lane_spawn in get_tree().get_nodes_in_group("car_spawn"):
+		if (
+			_in_player_view(lane_spawn)
+			and (
+				_distance_to_player(lane_spawn) < spawn_distance_in_view
+				or _distance_to_player(lane_spawn) > Global.PLAYER_VIEW_DISTANCE
+			)
+		):
+			_spwan_car(lane_spawn)
+
+
+func _in_player_view(node: Node3D) -> bool:
+	var direction = _player_car.global_position.direction_to(node.global_position)
+	var car_forward = -_player_car.global_basis.z
+
+	# Project onto XZ plane
+	direction.y = 0.0
+	car_forward.y = 0.0
+	direction = direction.normalized()
+	car_forward = car_forward.normalized()
+
+	var angle = rad_to_deg(direction.angle_to(car_forward))
+	return angle < spawn_angle_to_player
+
+
+func _distance_to_player(node: Node3D) -> float:
+	return _player_car.global_position.distance_to(node.global_position)
+
+
+func _despawn_cars():
+	for i in range(_controllers.size() - 1, -1, -1):
+		var controller := _controllers[i] as AiCarController
+		var car := controller.car
+		if (
+			(_in_player_view(car) and _distance_to_player(car) < spawn_distance_in_view)
+			or _distance_to_player(car) < spawn_distance_out_of_view
+		):
+			continue
+		car.queue_free()
+		_controllers.remove_at(i)
 
 
 func _spwan_car(lane_spawn: CarSpawnLane):
